a633927e7af685a1725e9bbd732cc88003c255f7
bxz5089 <69992026+bxz5089@users.noreply.github.com>
2022-04-09 10:00:54 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,30 +1,30 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
-    username: { 
-        type: String, 
-        unique: true,
-        required: true,
-        trim: true
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-    email: { 
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/] 
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/],
     },
     thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        }
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
     ],
     friends: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
     ],
   },
   {
@@ -35,12 +35,10 @@ const userSchema = new Schema(
   }
 );
 
-userSchema
-  .virtual('friendCount')
-  .get(function () {
-    return this.friends.length;
-  })
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-const User = model('user', userSchema);
+const User = model("user", userSchema);
 
 module.exports = User;
